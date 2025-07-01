823b42aaf8b4ec44510dc94c0f1cb721d0033af0
Aysenursvs <sivasligiln@gmail.com>
2025-07-01 17:04:57 +0300

    send many request like a bot

---
+++ like_bot.py
@@ -0,0 +1,14 @@
+import requests
+
+for _ in range(5):
+    response = requests.get("http://localhost/", headers={"User-Agent": "haha-bot", "Accept": "*/*"})
+
+    print(response.status_code)
+    print(type(response.content))
+    print(response.request.headers)
+    #print(response.text)
+    print(response.headers)
+    try:
+        print(response.json())
+    except ValueError:
+        print("JSON yanıtı alınamadı.")
